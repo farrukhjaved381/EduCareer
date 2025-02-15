38385f4e727b27406d0f857b4071300684af2af6
Muhammad Farrukh Javed <mfarrukhjaved381@gmail.com>
2025-02-15 09:42:19 +0500

    Initial upload of EduCareer project

---
+++ frontend/src/App.vue
@@ -0,0 +1,13 @@
+<template>
+  <DashboardLayout />
+</template>
+
+<script>
+import DashboardLayout from "./components/DashboardLayout.vue";
+
+export default {
+  components: {
+    DashboardLayout,
+  },
+};
+</script>
